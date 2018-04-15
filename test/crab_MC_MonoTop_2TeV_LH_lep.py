09c8983f2916128eb2d8577fd5c64abae09e01c1
Ryan Mueller <33radar@gmail.com>
2018-04-15 10:59:58 -0500

    adding crab configs

--- test/crab_MC_MonoTop_2TeV_LH_lep.py
+++ test/crab_MC_MonoTop_2TeV_LH_lep.py
@@ -2,15 +2,15 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 import os
 
 config = config()
-config.General.requestName = 'monotopTree_MT_1TeV_LH_13TeV_had_V11'
+config.General.requestName = 'monotopTree_MT_2TeV_LH_13TeV_lep_V11'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'run_B2GTTbarTreeMaker_MonoTopNew_Toolbox.py'
-config.JobType.maxMemoryMB = 2500
 #config.JobType.maxJobRuntimeMin = 2750
+config.JobType.maxMemoryMB = 2500
 
-config.Data.userInputFiles = open('mt_had_1tev_lh.txt').readlines()
+config.Data.userInputFiles = open('mt_lep_1tev_LH.txt').readlines()
 config.Data.inputDBS = 'global'
 config.JobType.inputFiles = [
 'PUweight_FinalJSON2016_PileupJSONFeb2017_Xsec69200nominal_MCRunIISummer16MiniAODv2_PUMoriond17.root',
@@ -40,9 +40,12 @@ os.environ['CMSSW_BASE'] + '/src/JMEAnalysis/JRDatabase/textFiles/Spring16_25nsV
 os.environ['CMSSW_BASE'] + '/src/JMEAnalysis/JRDatabase/textFiles/Spring16_25nsV10_MC/Spring16_25nsV10_MC_SF_AK8PFchs.txt'
 ]
 config.Data.splitting = 'FileBased'
-#config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = 3
 config.Data.outLFNDirBase = '/store/user/rymuelle/MonoTop/samples'
 config.Data.publication = False
 config.Site.storageSite = 'T3_US_FNALLPC'
+
+config.Data.ignoreLocality = True
+config.Site.whitelist = ['T3_US_FNALLPC']
+config.Site.ignoreGlobalBlacklist = True
